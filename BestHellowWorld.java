70d860571a65d8f8571be77b429cbb12b3ce6947
JohnnyLynch <alynch@ggc.edu>
2013-01-14 13:40:03 -0500

    BestHellowWorld.java was add.

---
+++ BestHellowWorld.java
@@ -0,0 +1,13 @@
+public class BestHellowWorld{
+
+	public static void main (String[] args){
+
+	string bestHellowWorld = "BESTHELLOWWORLD!!!";
+
+	for (int i = bestHellowWorld.length(); i > 0; i--){
+		System.out.println(bestHellowWorld.subString(i))  	
+	} 
+	
+	}
+
+}
